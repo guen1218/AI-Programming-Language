0321a1397043b6f48e58807891dc192d06228cb1
DESKTOP-QQTHH1M\USER <radio071218@gmail.com>
2026-05-15 11:29:13 +0900

    no message

--- 과제/Hangman/hangman.py
+++ 과제/Hangman/hangman.py
@@ -5,7 +5,7 @@ class Hangman:
     MASK_CAHR = '_'
     RIGHT = 1
     WRONG = 0
-    EXIST = -1
+    ERROR = -1
     WIN = 1
     LOOSE = 0
     CONTINUE = -1
@@ -13,12 +13,19 @@ class Hangman:
         self.word = random.choice(word_list)
         self.display_word = Hangman.MASK_CAHR * len(self.word)
         self.num_try = 0
+        self.input_letters = []
+        self.error_status = ""
     def check_letter(self, letter):
+        letter = letter.lower()
+        if letter in self.input_letters:
+            self.error_status = f"이미 입력한 알파벳입니다. {self.input_letters}"
+            return self.ERROR
+        
+        self.input_letters.append(letter)
+
         if self.word.count(letter) > 0:
             # 있는 문자이면 display_word에서 위치를 찾아 수정
             for i in range(len(self.word)):
-                if self.word[i] == self.display_word[i]:
-                    return self.EXIST
                 if self.word[i] == letter:
                     self.display_word = self.display_word[:i] + letter + self.display_word[i+1:]
             return Hangman.RIGHT
--- 과제/Hangman/hangman_console.py
+++ 과제/Hangman/hangman_console.py
@@ -9,21 +9,24 @@ print(f'{hangman.display_word} {len(hangman.word)}글자')
 
 while True:
     letter = input(">> 알파벳 입력 : ")
+    if letter.isalpha():
+        result = hangman.check_letter(letter)
 
-    result = hangman.check_letter(letter)
+        if result == Hangman.RIGHT:
+            print(f'정답 : {hangman.display_word}')
+        elif result == Hangman.WRONG:
+            print (f"오답 : {hangman.num_try}회 시도")
+        elif result == Hangman.ERROR:
+            print (hangman.error_status)
+            continue
 
-    if result == Hangman.RIGHT:
-        print(f'정답 : {hangman.display_word}')
-    elif result == Hangman.WRONG:
-        print (f"오답 : {hangman.num_try}회 시도")
-    elif result == Hangman.EXIST:
-        print (f"다시하세요")
-
-    #승패 확인
-    result = hangman.is_win()
-    if result == Hangman.WIN:
-        print(f"You win ~~!~!~!~~~~ : {hangman.num_try}회 시도")
-        break
-    elif result == Hangman.LOOSE: 
-        print (f"You loose ~~!~!~~~~ : {hangman.word}")
-        break
+        #승패 확인
+        result = hangman.is_win()
+        if result == Hangman.WIN:
+            print(f"You win ~~!~!~!~~~~ : {hangman.num_try}회 시도")
+            break
+        elif result == Hangman.LOOSE: 
+            print (f"You loose ~~!~!~~~~ : {hangman.word}")
+            break
+    else:
+        print("알파벳을 입력하시요")
