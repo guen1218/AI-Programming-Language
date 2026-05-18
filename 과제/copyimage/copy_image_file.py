8376dc33b251c7e9f6fe56fe99860d1834f8094c
이근휘 <131231974+guen1218@users.noreply.github.com>
2026-05-18 23:10:44 +0900

    Add files via upload

---
+++ 과제/copyimage/copy_image_file.py
@@ -0,0 +1,18 @@
+import os
+
+def copy_image_file(file):
+    buffer = 4096 # 4KB 
+    name, ext = os.path.splitext(file) # 확장자랑 본문을 나눔
+    with open(file, 'rb') as fr: 
+        with open(name+"_copy"+ext, 'wb') as fw:
+            while True:
+                chunk = fr.read(buffer) # 4kb 만큼만 읽음
+
+                if not chunk:
+                    break # 청크가 비어있으면(더이상 읽을 게 없다면(다 읽었다면)) break
+
+                fw.write(chunk) # 넣기
+
+copy_image_file("i_am_gaegosu.jpg")
+copy_image_file("iamgosu.gif")
+copy_image_file("yaho.png")
