4dc3a42e96807e0e1a9a7d75f3da7a44d7d2bd1b
DESKTOP-QQTHH1M\USER <radio071218@gmail.com>
2026-06-10 09:36:41 +0900

    no message

--- 과제/Member/member.py
+++ 과제/Member/member.py
@@ -2,23 +2,31 @@
 # 데이터 모델 정의 : Member
 class Member:
     def __init__(self, id, password, name):
-        self.__member_no = 0
         self.__id = id
         self.__password = password
         self.__name = name
+        self.__address = None
+        self.__phone = None
 
-    def get_member_no(self):
-        return self.__member_no
     def get_id(self):
         return self.__id
     def get_password(self):
         return self.__password
     def get_name(self):
         return self.__name
+    def get_address(self):
+        return self.__address
+    def get_phone(self):
+        return self.__phone
+    
     def set_id(self, id):
         self.__id = id
     def set_password(self, password):
         self.__password = password
+    def set_address(self, address):
+        self.__address = address
+    def set_phone(self, phone):
+        self.__phone = phone
     
     def __str__(self):
-        return f'{self.__member_no}\t{self.__id}\t{self.__name}\t{self.__password}'   
+        return f'{self.__id}\t{self.__name}\t{self.__phone}\t{self.__address}'   
